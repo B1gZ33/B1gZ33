2de4196f4fa30b32a1a662d81fde1d2658062fc9
B1gZ33 <169104114+B1gZ33@users.noreply.github.com>
2024-08-18 23:03:40 +1200

    Add files via upload
    these are all the files  I have used for my whole project

---
+++ car.tsn.gd
@@ -0,0 +1,28 @@
+extends VehicleBody3D
+
+const MAX_STEER = 0.15
+const ENGINE_POWER = 1000
+
+@onready var camera_3d = $"Camera Pivot/Camera3D"
+@onready var camera_pivot = $"Camera Pivot"
+
+
+
+
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	steering = move_toward(steering, Input.get_axis("ui_right", "ui_left") * MAX_STEER, delta * 2.5)
+	engine_force = Input.get_axis("ui_down", "ui_up") * ENGINE_POWER
+	
+	
+  
+
